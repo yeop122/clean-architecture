76c0628487bd4c38c8abd615f450b511e942d721
yp122 <yp1222@koast.net>
2024-01-14 03:28:02 +0900

    mapstructure 오류나서 modelmapper 사용

--- template/src/main/java/com/example/demo/member/adaptor/in/web/MemberController.java
+++ template/src/main/java/com/example/demo/member/adaptor/in/web/MemberController.java
@@ -1,11 +1,13 @@
 package com.example.demo.member.adaptor.in.web;
 
+import com.example.demo.common.util.modelmapper.ModelMapperWrapper;
 import com.example.demo.member.adaptor.in.dto.RegisterMemberRequest;
 import com.example.demo.member.application.port.in.MemberUseCase;
 import com.example.demo.member.domain.Member;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,12 +22,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     private final MemberUseCase memberUseCase;
-    private final RegisterMemberMapper registerMemberMapper;
+//    private final RegisterMemberMapper registerMemberMapper;
 
     @PostMapping
     public Member addUser(@RequestBody RegisterMemberRequest registerMemberRequest) {
 
-        Member member = registerMemberMapper.dtoToDomain(registerMemberRequest);
+//        Member member = registerMemberMapper.dtoToDomain(registerMemberRequest);
+        Member member = ModelMapperWrapper.map(registerMemberRequest, Member.class);
         return memberUseCase.registerMember(member);
     }
 
--- template/src/main/java/com/example/demo/member/adaptor/out/persistence/MemberDAO.java
+++ template/src/main/java/com/example/demo/member/adaptor/out/persistence/MemberDAO.java
@@ -1,10 +1,13 @@
 package com.example.demo.member.adaptor.out.persistence;
 
+import com.example.demo.common.util.modelmapper.ModelMapperWrapper;
 import com.example.demo.member.adaptor.out.entity.MemberEntity;
 import com.example.demo.member.application.port.out.MemberPort;
 import com.example.demo.member.domain.Member;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -12,7 +15,7 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 public class MemberDAO implements MemberPort {
 
-    private final MemberMapper memberMapper;
+//    private final MemberMapper memberMapper;
     // JPA
     private final MemberJpaRepository memberJpaRepository;
     // JDBC Template
@@ -24,9 +27,11 @@ public class MemberDAO implements MemberPort {
      * @return saved entity
      */
     public Member save(Member member) {
-        MemberEntity memberEntity = memberMapper.toEntity(member);
+//        MemberEntity memberEntity = memberMapper.toEntity(member);
+        MemberEntity memberEntity = ModelMapperWrapper.map(member, MemberEntity.class);
         MemberEntity savedEntity = memberJpaRepository.save(memberEntity);
-        return memberMapper.toDomain(savedEntity);
+//        return memberMapper.toDomain(savedEntity);
+        return ModelMapperWrapper.map(savedEntity, Member.class);
     }
 
     /**
@@ -35,7 +40,9 @@ public class MemberDAO implements MemberPort {
      * @return integer
      */
     public int save2(Member member) {
-        MemberEntity memberEntity = memberMapper.toEntity(member);
+//        MemberEntity memberEntity = memberMapper.toEntity(member);
+        ModelMapper modelMapper = new ModelMapper();
+        MemberEntity memberEntity = modelMapper.map(member, MemberEntity.class);
         return memberJdbcRepository.save(memberEntity);
     }
 }
